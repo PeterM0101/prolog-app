714e8b29f6c612fef56177c47c7cb197f6fd2d58
Peter Maliar <peter.maliar@tabsandspaces.agency>
2024-05-04 17:52:26 +0300

    Made additional changes

--- styles/theme.ts
+++ styles/theme.ts
@@ -249,6 +249,7 @@ export const theme = {
 };
 
 export function color(name: keyof Theme["color"], shade: keyof Color) {
+    // add comments
     return ({theme}: { theme: Theme }) => theme.color[name][shade];
 }
 
